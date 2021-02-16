904d63f65b0263f7d95e5584be7066af4bb25737
Maxim Urschumzew <m.urschumzew@outlook.com>
2021-02-16 22:21:02 +0100

    Try again.

--- scripts/custom-julia-2.sh
+++ scripts/custom-julia-2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-julia --color=yes -e "using Pkg; Pkg.build(verbose=true); end"
+julia --color=yes -e "using Pkg; Pkg.build(verbose=true);"
 # run tests
-julia --check-bounds=yes --color=yes -e "using Pkg; Pkg.test(coverage=true); end"
+julia --check-bounds=yes --color=yes -e "using Pkg; Pkg.test(coverage=true);"
 # coverage
    # 'julia --color=yes -e "if VERSION < v\"0.7.0-DEV.5183\"; cd(Pkg.dir(\"${JL_PKG}\")); else using Pkg; end; Pkg.add(\"Coverage\"); using Coverage; Codecov.submit(process_folder())"'
