082c6c9f3dd483219cae2b22cf324b810695a55e
Maxim Urschumzew <m.urschumzew@outlook.com>
2021-02-16 22:58:55 +0100

    Try get coverage working.

--- scripts/custom-julia-build.sh
+++ scripts/custom-julia-build.sh
@@ -3,6 +3,6 @@
 julia --color=yes -e "using Pkg; Pkg.build(verbose=true);"
 
 # run tests
-julia -O0 --compile=min --check-bounds=yes --color=yes -e "using Pkg; Pkg.test(coverage=true);"
+julia --check-bounds=yes --color=yes -e "using Pkg; Pkg.test(coverage=true);"
 # coverage
-julia -O0 --compile=min --color=yes -e "using Pkg; Pkg.add(\"Coverage\"); using Coverage; Codecov.submit(process_folder())"
+julia --color=yes -e "using Pkg; Pkg.add(\"Coverage\"); using Coverage; Codecov.submit(process_folder())"
