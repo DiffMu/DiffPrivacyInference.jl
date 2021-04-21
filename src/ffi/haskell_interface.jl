355c1370b8d2c3b0fe332d6cead66e1fbbf6818b
Maxim Urschumzew <m.urschumzew@outlook.com>
2021-04-21 18:49:25 +0200

    Convert term to string and pass to haskell typechecker.

--- src/ffi/haskell_interface.jl
+++ src/ffi/haskell_interface.jl
@@ -2,7 +2,9 @@
 using Base.Libc.Libdl
 
 
-function typecheck_hs(myf)
+function typecheck_hs_from_dmterm(term::DMTerm)
+    str = string(term)
+
     # load the shared library
     # Note, the library has to be available on a path in $LD_LIBRARY_PATH
     dm = Libdl.dlopen("libdiffmu-wrapper")
@@ -11,12 +13,12 @@ function typecheck_hs(myf)
     init = Libdl.dlsym(dm, :wrapperInit)
     exit = Libdl.dlsym(dm, :wrapperExit)
     test = Libdl.dlsym(dm, :test)
-    typecheckFromDMTerm = Libdl.dlsym(dm, :typecheckFromDMTerm)
+    typecheckFromDMTerm = Libdl.dlsym(dm, :typecheckFromCString_DMTerm)
 
     # call the library
     ccall(init, Cvoid, ())
     # ccall(test, Cvoid, ())
-    ccall(typecheckFromDMTerm, Cvoid, (Cstring,), "Please use this")
+    ccall(typecheckFromDMTerm, Cvoid, (Cstring,), str)
     ccall(exit, Cvoid, ())
 
     # unload the library
