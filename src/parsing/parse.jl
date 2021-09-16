06de4d4b5e93a9f66fe201ec64c4090eb6b0762e
ooovi <githooovi@posteo.de>
2021-09-16 20:16:40 +0200

    fixing non-end-of-block subgrad

--- src/parsing/parse.jl
+++ src/parsing/parse.jl
@@ -365,6 +365,9 @@ function exprs_to_dmterm(exs, ln, scope = ([],[],[], false)) :: DMTerm
                         end
                         inast = Meta.parseall(read(args[1], String), filename = args[1])
                         return exprs_to_dmterm([inast.args; tail], inast.args[1], scope)
+                    elseif f == :subtract_gradient!
+                        @assert length(args) == 2 "wrong number of arguments for subtract_gradient!: $ex in $(ln.file) line $(ln.line)"
+                        return dmsubgrad(exprs_to_dmterm(args[1], ln, scope), exprs_to_dmterm(args[2], ln, scope))
                     elseif isempty(tail)
                         if f in F
                             error("recursive call of $f in in $(ln.file) line $(ln.line)")
