281c3c178a081dade79aec24902f8bccd9d62c54
ooovi <githooovi@posteo.de>
2021-11-01 14:13:52 +0100

    non-mutating copying hack

--- src/builtins.jl
+++ src/builtins.jl
@@ -23,9 +23,7 @@ BlackBox(T::DataType) = T
 
 "A wrapper for Flux.Params, so we can control mutation."
 mutable struct DMModel
-   model
-   params :: Flux.Params
-   DMModel(m) = new(m, Flux.params(m))
+   model # a flux model
 end
 
 "A wrapper for Zygote.Grads, so we can control what you can do with gradients."
@@ -35,21 +33,28 @@ end
 
 
 "Subtract the gradients from the parameters."
-function subtract_gradient!(ps::DMModel, gs::DMGrads)
-   for p in ps.params
-      p .-= gs.grads[p]
+function subtract_gradient(m::DMModel, gs::DMGrads) :: DMModel
+   cm = deepcopy(m.model)
+   cp = Flux.params(cm)
+   p = Flux.params(m.model)
+   for i in 1:length(Flux.params(m).order.data)
+      cp[i] .-= gs.grads[p[i]]
    end
+   return DMModel(cm)
 end
 
+copy_grad(g::DMGrads) :: DMGrads = DMGrads(Zygote.Grads(IdDict(g.grads.grads), g.grads.params))
 
 "Make the input gradient DP by applying the gaussian mechanism."
-function gaussian_mechanism!(s::Real, ϵ::Real, δ::Real, f::DMGrads)
+function gaussian_mechanism(s::Real, ϵ::Real, δ::Real, f::DMGrads) :: DMGrads
+   cf = copy_grad(f)
    noise!(ff) = ff + rand(Normal(0, (2 * log(1.25/δ) * s^2) / ϵ^2))
-   map!(ff -> noise!.(ff), f.grads, f.grads) # apply noise element-wise
+   map!(ff -> noise!.(ff), cf.grads, cf.grads) # apply noise element-wise
+   return cf
 end
 
 "Clip the gradient, i.e. scale by 1/norm(g) if norm(g)>1."
-function clip!(l::Norm, g::DMGrads)
+function clip(l::Norm, g::DMGrads) :: DMGrads
 
     p = @match l begin
         L1 => 1
@@ -57,10 +62,14 @@ function clip!(l::Norm, g::DMGrads)
         L∞ => Inf
     end
 
-    n = norm(g.grads, p)
+    cg = copy_grad(g)
+
+    n = norm(cg.grads, p)
 
     if n > 1
-       g.grads .*= (1/n)
+       cg.grads .*= (1/n)
     end
+
+    return cg
 end
 
--- test/flux_dp/flux_dp.jl
+++ test/flux_dp/flux_dp.jl
@@ -1,30 +1,33 @@
 import Flux
 
-function unbounded_gradient(loss, model::DMModel, d::Vector, l::Vector) :: BlackBox()
-   gs = Flux.gradient(model.params) do
-           loss(d,l)
+function unbounded_gradient(model::DMModel, d::Vector, l::Vector) :: BlackBox()
+   gs = Flux.gradient(Flux.params(model.model)) do
+           loss(d,l,model)
         end
    return DMGrads(gs)
 end
 
-function train_dp!(loss::NoData(), model::NoData(DMModel), data, labels, eps::NoData(), del::NoData(), eta::NoData()) :: Priv()
-   (n,) = size(data)
-   for i in 1:n
-      d = data[i,:]
-      l = labels[i]
-      gs = unbounded_gradient(loss, model, d, l)
+function init_model() :: BlackBox()
+   DMModel(Flux.Chain(Flux.Dense(20,100,Flux.σ),Flux.Dense(100,10),Flux.softmax))
+end
 
-      clip!(L2,gs)
-      gaussian_mechanism!(1/n, eps, del, gs)
-      subtract_gradient!(model,gs)
+loss(x,y,model) :: BlackBox() = Flux.mse(model.model(x),y)
+
+function train_dp(data, labels, eps::NoData(), del::NoData(), eta::NoData()) :: Priv()
+   model = init_model()
+   (n,m) = size(data)
+   for _ in 1:eta
+       for i in 1:n
+          d = data[i,:]
+          l = labels[i]
+          gs = unbounded_gradient(model, d, l)
+
+          gs = clip(L2,gs)
+          gs = gaussian_mechanism(1/n, eps, del, gs)
+          model = subtract_gradient(model,gs)
+       end
    end
+   model
 end
 
 
-model = DMModel(Flux.Chain(Flux.Dense(784,32,Flux.σ),Flux.Dense(32,10),Flux.softmax))
-loss(x,y) = Flux.mse(model.model(x),y)
-#data = rand(1000,784)
-#labels = [ones(10) for _ in 1:1000]
-
-
-(data, labels) -> train_dp!(loss, model, data, labels, 1, 1, 1)
