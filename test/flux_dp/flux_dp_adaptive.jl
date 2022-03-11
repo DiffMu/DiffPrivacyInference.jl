e325f084f6da823927955cdb37449256777fc1df
ooovi <githooovi@posteo.de>
2022-03-11 18:21:55 +0100

    updating adaptive clipping

--- test/flux_dp/flux_dp_adaptive.jl
+++ test/flux_dp/flux_dp_adaptive.jl
@@ -1,43 +1,12 @@
+module AdaptiveClipping
+
 # this code is checkable with our typechecker. the inference result will be the type of the last function
 # in the file, containing the inferred privacy bound as well as constraints on the input variables.
-
-# only imports are permitted, so you cannot use functions from other packages without qualifying them.
-# qualified names cannot be used in functions whose body is supposed to be checked by us, but only
-# in black-box functions that will be ignored by the type checker.
-import Flux
-
-# a black-box function using qualified names to call functions from Flux.
-# the typechecker will just ignore the body of this function and assign infinite sensitivity to all
-# arguments. note that if you mutate any of the arguments, the typechecking result will be invalid,
-# but the typechecker will not warn you about it.
-function unbounded_gradient(model::DMModel, dd) :: BlackBox()
-   l = dd[end]
-   d = dd[1:end-1]
-   gs = Flux.gradient(Flux.params(model.model)) do
-           loss(d,l,model)
-        end
-   return DMGrads(gs)
-end
-
-# a black-box function initializing a Flux neural network. To make it work with typecheckable code,
-# the model has to be wrapped in our `DMModel` struct.
-function init_model() :: BlackBox()
- DMModel(Flux.Chain(
-         Flux.Dense(28*28,40, Flux.relu),
-         Flux.Dense(40, 10),
-         Flux.softmax))
-end
-
-# the loss function for our training. using a function from Flux, so it's a black-box too.
-loss(X, y, model) :: BlackBox() = Flux.crossentropy(model.model(X), y)
-
-
-# the only function that is actually typechecked: the gradient descent training algorithm.
 
 # give a one-hot vector for rows that change when clipped (i.e. that have norm > 1)
 function filter_box(b::Real, xs::Matrix) :: BlackBox()
    cxs = map(x -> clip(L2, b*x), eachrow(xs))
-   clone([(rm == rn ? 1. : 0.) for (rm,rn) in zip(eachrow(xs), (cxs))])
+   clone([(rm == rn ? 0. : 1.) for (rm,rn) in zip(eachrow(xs), (cxs))])
 end
 
 # given a vector of clipping parameters, find the index of the one for which 90% of the samples
@@ -49,78 +18,66 @@ function select_clipping_param(xs::Matrix{<:Real}, eps::Real, bs::Vector{<:Real}
       xxs = unbox(filter_box(b,xs),Vector{<:Real},d)
       1.0*count(x -> x==1., xxs) #(make it real...)
    end
-   (dim, _) = size(xs)
-   target = 0.9 * dim
+   (dm, _) = size(xs)
+   target = 0.9 * dm
    fs = map(b -> (xs -> test_scale(b,xs)), bs)
    above_threshold(fs, eps, xs, target)
 end
-
-function col_means(m, eps::NoData(), del::NoData(), bs::NoData()) :: Priv() 
+       
+function col_means(m::Matrix{<:Real}, eps::NoData(), del::NoData(), bs::NoData(Vector{<:Real})) :: Priv() 
    # compute dp mean of a 1-row matrix (basically a col vector...)
-   function dp_col_mean(v) :: Priv()
-     (nrows,_) = size(v)
+   function dp_col_mean(v::Matrix{<:Real}) :: Priv()
+     # find a scalar b s.t. 90% of rows of b*v remain unclipped
      bp = select_clipping_param(v, eps/2, bs)
      b = bs[bp]
-     clipped = map_rows(x -> clip(L1,b*x), v)
-     norm_convert!(clipped)
-     sm = fold((x,y)->x+y, 0., clipped) / nrows
-     g = gaussian_mechanism(1/nrows, eps/2, del, sm)
-     disc(g)
-   end
 
+     # scale and clip
+     clipped = map_rows(x -> clip(L1,disc(b)*x), v)
+     norm_convert!(clipped)
+
+     # compute mean of clipped vector
+     (nrows,_) = size(v)
+     sm = fold((x,y)->x+y, 0., clipped) / nrows
+
+     # add noise to mean
+     g = gaussian_mechanism(1/nrows, eps/2, del, sm)
+     # re-scale
+     disc(1/b) * disc(g)
+   end
    reduce_cols(dp_col_mean, m)
 end
 
+# subtract the mean (given as a 1-row matrix) of each column of mat from each entry in said column.
 function center(mat::Matrix, means::Matrix)
-   center_col(col::Vector, mean::Vector) = map(x -> x - mean[0], col)
+   center_col(col::Vector, mean::Vector) = map(x -> x - mean[1], col)
    map_cols_binary(center_col, mat, means)
 end
 
-function col_scale_params(centered, eps::NoData(), del::NoData(), bs::NoData(Vector), means::Matrix) :: Priv()
+# get the vector of clipping parameters the SVT determines for each column of `centered`.
+function col_scale_params(centered, eps::NoData(), del::NoData(), bs::NoData(Vector)) :: Priv()
    function select(col) :: Priv()
       select_clipping_param(col, eps, bs)
    end
    reduce_cols(select, centered)
 end
-
-function normalize(scales::Matrix, centered::Matrix)
-   scale_col(col::Vector, scale::Vector) = map(x -> x / scale[0], col)
-   map_cols_binary(scale_col, centered, scales)
-end
-   
-function main(m, eps::NoData(), del::NoData(), bs::NoData(Vector), eta::NoData(), k::NoData(), b::NoData()) :: Priv()
-   means = col_means(m, eps, del, bs)
-   centered = center(m, vec_to_row(means))
-   scales = col_scale_params(centered, eps, del, bs, vec_to_row(means))
-   normalize(scales, centered)
-   train_dp(m, eps, del, eta, k, b)
-end
-
-
+# the only function that is actually typechecked: the gradient descent training algorithm.
 # we're only interested in the privacy of the `data` and `labels` inputs so all other parameters
 # get a `NoData()` annotation. it's a privacy function, so we annotate it with `Priv()`.
-function train_dp(data, eps::NoData(), del::NoData(), eta::NoData(), k::NoData(Integer), b::NoData(Integer)) :: Priv()
-   # initialize a Flux model.
-   n_params = 31810
-   model = unbox(init_model(), DMModel, n_params)
+function train_dp_bounded_grad(data, eps::NoData(), del::NoData(), eta::NoData(), k::NoData(Integer), b::NoData(Integer), grad::NoData(Function), model::NoData(DMModel)) :: Priv()
    (dim, _) = size(data)
    for i in 1:k*(dim/b)
-      D, _ = sample(b, data, data)
-      G = zero_gradient(model)
+      #D, _ = sample(b, data, data)
+      #G = zero_gradient(model)
 
-
-      for j in 1:b
+      #for j in 1:b
          # compute the gradient at the i-th data point
-         d = D[j,:]
-         l = L[j,:]
-         gs = unbox(unbounded_gradient(model, d, l), DMGrads, n_params)
+         #da = D[j,:]
+         da = data[i,:]
+         G = grad(model, da)
+         #gs = grad(model, da)
 
-         # clip the gradient
-         clip!(L2,gs)
-         norm_convert!(gs)
-
-         G = sum_gradients(gs,G)
-      end
+      #   G = sum_gradients(gs,G)
+      #end
 
       # apply the gaussian mechanism to the gradient.
       # we scale the gradient prior to this to bound it's sensitivity to 2/dim, so the noise
@@ -133,6 +90,27 @@ function train_dp(data, eps::NoData(), del::NoData(), eta::NoData(), k::NoData(I
       scale_gradient!(eta, G)
       subtract_gradient!(model, G)
    end
-   model
+   return
 end
 
+
+# scale each column of `centered` by the corresponding scalar in `scales`
+function normalize(scales::Matrix, centered::Matrix)
+   scale_col(col::Vector, scale::Vector) = map(x -> x / scale[1], col)
+   map_cols_binary(scale_col, centered, scales)
+end
+ 
+function main(m, eps::NoData(), del::NoData(), bs::NoData(Vector), eta::NoData(), k::NoData(), b::NoData(), grad::Function, model::NoData(DMModel)) :: Priv()
+   means = col_means(m, eps, del, bs)
+   centered = center(m, vec_to_row(means))
+   scales = col_scale_params(centered, eps, del, bs)
+#   n = normalize(vec_to_row(scales), centered)
+#   (nrws, _) = size(m)
+
+#   gaussian_mechanism(nrws, eps, del, n)# centered)
+   #train_dp_bounded_grad(n, eps, del, eta, k, b, grad, model)
+   #return
+end
+
+
+end
