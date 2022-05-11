8e1b01f95cb7056f5ea061fc2c2263be40440391
ooovi <githooovi@posteo.de>
2022-05-11 13:18:00 +0200

    deleting old things

--- scripts/custom-julia-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-julia --color=yes -e "using Pkg; Pkg.build(verbose=true);"
-
-# run tests
-julia -O0 --compile=min --check-bounds=yes --color=yes -e "using Pkg; Pkg.test(coverage=true);"
-# coverage
-julia -O0 --compile=min --color=yes -e "using Pkg; Pkg.add(\"Coverage\"); using Coverage; Codecov.submit(process_folder())"
--- test/runtests.jl
+++
@@ -1,6 +0,0 @@
-using DiffPrivacyInference, Test
-# say include("test/runtests.jl") to run
-
-include("parsing.jl")
-include("check_eval_simpl.jl")
-include("errors.jl")
